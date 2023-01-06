ef8570f1d5b4dd4b2c757e94ec8d9287c231b61c
Ignacio <marioig.navarrete@gmail.com>
2023-01-06 00:26:03 -0300

    change sending email

--- helpers/emailOlvidePassword.js
+++ helpers/emailOlvidePassword.js
@@ -1,32 +1,52 @@
-import nodemailer from 'nodemailer';
+import sgMail from "@sendgrid/mail";
 
 const emailOlvidePassword= async (datos) => {
-    const transport = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
-        }
+    const { email, nombre, token } = data;
+
+  const htmlRestablecer = `
+  <div style="font-family: 'Open Sans','Roboto','Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 16px; color: #757575; line-height: 150%; letter-spacing: normal;">
+  <div style="background: #4f46e5; padding: 50px 10px;">
+  <div style="max-width: 600px; margin: auto;">
+  <div style="background: white; padding: 15px 30px 25px 30px; border-radius: 5px;">
+  <div style="text-align: center; margin: 20px 0 30px;"><span style="font-weight: bold; color: #4f46e5; font-size: 30px; margin-left: 10px;">Restablecer password en Administrador de Pacientes de Clínica</span></div>
+  <p style="color: #757575; font-family: 'Open Sans', Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal;">Hola ${nombre}&nbsp;</p>
+  <p style="color: #757575; font-family: 'Open Sans', Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal;">Has solicitado restablecer tu password en Administrador de Pacientes de Clínica</p>
+  <p>Sigue el siguiente enlace para generar un nuevo password:&nbsp;</p>
+  <p style="color: #757575; font-family: 'Open Sans', Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal;"><a style="background: #4f46e5; color: white; font-weight: 500; display: inline-block; padding: 10px 35px; margin: 6px 8px; text-decoration: none; border-radius: 2px;" href="${process.env.FRONTEND_URL}/olvide-password/${token}" target="_blank" rel="noopener">Restablecer password</a></p>
+  <p style="color: #757575; font-family: 'Open Sans', Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal;">El enlace expira inmediatamente cuando se abre.</p>
+  <p style="color: #757575; font-family: 'Open Sans', Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal;">Si tu no solicitaste este cambio, puedes ignorar el mensaje.</p>
+  </div>
+  </div>
+  </div>
+  <div style="background: #4f46e5; color: white; font-size: 12px; padding: 30px 10px 30px 10px;">
+  <div style="max-width: 600px; margin: auto; text-align: center;"><hr style="border: 1px solid #f2f2f2;">
+  <p style="font-style: italic; margin-bottom: 0;">Copyright &copy; 2023 Administrador de Pacientes de Clínica, All rights reserved.</p>
+  <hr style="border: 1px solid #f2f2f2;"></div>
+  <div class="yj6qo">&nbsp;</div>
+  <div class="adL">&nbsp;</div>
+  </div>
+  <div class="adL" style="color: #757575; font-family: 'Open Sans', Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal;">&nbsp;</div>
+  </div>
+`;
+
+  sgMail.setApiKey(process.env.API_KEY);
+
+  const msg = {
+    to: email,
+    from: "marioig.navarrete@gmail.com",
+    subject: "Restablecer password - Administrador de Pacientes de Clínica.",
+    text: "Administrador de Pacientes de Clínica",
+    html: htmlRestablecer,
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email Enviado");
+    })
+    .catch((error) => {
+      console.error(error);
     });
 
-    const { email, nombre, token} = datos;
-    
-    console.log(token);
-    
-    const info = await transport.sendMail({
-        from: "APH - Administrador de Pacientes de Hospital",
-        to: email,
-        subject: 'Reestablece tu contraseña en APH',
-        text: 'Reestablece tu contraseña en APH',
-        html: `<p>Hola ${nombre}, has solicitado reestablecer tu contraseña </p>
-            <p>Sigue el siguiente enlace para cambiar tu contraseña:
-            <a href="${process.env.FRONTEND_URL}/olvide-password/${token}">Reestablecer Contraseña</a> </p>
-
-            <p>Si no solicitaste cambio de contraseña, puedes ignorar este mensaje</p>
-        `
-    });
-    console.log("Mensaje enviado: %s", info.messageId);
 }
 
 export default emailOlvidePassword;
--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -1,30 +1,53 @@
-import nodemailer from 'nodemailer';
+import sgMail from "@sendgrid/mail";
 
 const emailRegistro = async (datos) => {
-    const transport = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
-        }
-    });
+    const { email, nombre, token } = datos;
 
-    const { email, nombre, token} = datos;
-    
-    const info = await transport.sendMail({
-        from: "APH - Administrador de Pacientes de Hospital",
-        to: email,
-        subject: 'Valida tu cuenta en APH',
-        text: 'Valida tu cuenta en APH',
-        html: `<p>Hola ${nombre}, comprueba tu cuenta en APH. </p>
-            <p>Tu cuenta ya casi está lista, solo debes comprobarla en el siguiente enlace:
-            <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar Cuenta</a> </p>
+    console.log(email);
 
-            <p>Si no creaste esta cuenta, puedes ignorar este mensaje</p>  
-        `
-    });
-    console.log("Mensaje enviado: %s", info.messageId);
+    const htmlConfirmar = `
+    <div style="font-family: 'Open Sans','Roboto','Helvetica Neue',Helvetica,Arial,sans-serif; font-size: 16px; color: #757575; line-height: 150%; letter-spacing: normal;">
+    <div style="background: #4f46e5; padding: 50px 10px;">
+    <div style="max-width: 600px; margin: auto;">
+    <div style="background: white; padding: 15px 30px 25px 30px; border-radius: 5px;">
+    <div style="text-align: center; margin: 20px 0 30px;"><span style="font-weight: bold; color: #4f46e5; font-size: 30px; margin-left: 10px;">APC - Administrador de Pacientes de Clínica</span></div>
+    <p style="color: #757575; font-family: 'Open Sans', Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal;">Hola ${nombre} comprueba tu cuenta en Administrador de Pacientes de Clínica.</p>
+    <p style="color: #757575; font-family: 'Open Sans', Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal;">Tu cuenta ya esta casi lista, solo debes comprobarla en el siguiente enlace.</p>
+    <p style="color: #757575; font-family: 'Open Sans', Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal;">Por favor confirme su correo electr&oacute;nico (<a href="mailto:${email}" target="_blank" rel="noopener">${email}</a>) haciendo clic en el bot&oacute;n de abajo.</p>
+    <p><a style="background: #4f46e5; color: white; font-weight: 500; display: inline-block; padding: 10px 35px; margin: 6px 8px; text-decoration: none; border-radius: 2px;" href="${process.env.FRONTEND_URL}/confirmar-cuenta/${token}" target="_blank" rel="noopener">Confirmar</a></p>
+    <p style="color: #757575; font-family: 'Open Sans', Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal;">El enlace expira inmediatamente cuando se abre.</p>
+    <p style="color: #757575; font-family: 'Open Sans', Roboto, 'Helvetica Neue', Helvetica, Arial, sans-serif; font-size: 16px; letter-spacing: normal;">Si tu no creastes esta cuenta, puedes ignorar el mensaje.</p>
+    </div>
+    </div>
+    </div>
+    <div style="background: #4f46e5; color: white; font-size: 12px; padding: 30px 10px 30px 10px;">
+    <div style="max-width: 600px; margin: auto; text-align: center;"><hr style="border: 1px solid #f2f2f2;">
+    <p style="font-style: italic; margin-bottom: 0;">Copyright &copy; 2023 Administrador de Pacientes de Clínica, All rights reserved.</p>
+    <hr style="border: 1px solid #f2f2f2;"></div>
+    <div class="yj6qo">&nbsp;</div>
+    <div class="adL">&nbsp;</div>
+    </div>
+    <div class="adL">&nbsp;</div>
+    </div>
+    `;
+  
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  
+    const msg = {
+      to: email,
+      from: "marioig.navarrete@gmail.com",
+      subject: "Compruebe su cuenta en Administrador de Pacientes de Clínica",
+      text: "Administrador de Pacientes de Clínica",
+      html: htmlConfirmar,
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        console.log("Email Enviado");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 }
 
 export default emailRegistro;
